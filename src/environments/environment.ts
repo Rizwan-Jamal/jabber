24763e150e33e000e68b94e991339d653df4fffd
Mohammad Umair Khan <mohammad.umair.k@gmail.com>
2017-07-31 22:20:11 +0500

    feat: add firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBFYmGGh0NO8paK8uuUwVQ8rx3nPtx-1m4',
+    authDomain: 'jabber-f2b28.firebaseapp.com',
+    databaseURL: 'https://jabber-f2b28.firebaseio.com',
+    storageBucket: 'abber-f2b28.appspot.com',
+    projectId: 'jabber-f2b28',
+    messagingSenderId: '988481702222'
+  }
 };
