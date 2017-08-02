d9d2f8baee4d6db44415f9274e7e418d7ee27031
Mohammad Umair Khan <mohammad.umair.k@gmail.com>
2017-08-02 17:32:47 +0500

    feat: enter the MadBot! :rage:

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,32 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.triggerBot = functions.database.ref('/messages/{pushId}')
+  .onWrite(event => {
+    if(event.data.val().name !== 'MadBot'){
+      const reply = getBotMessage(event.data.val().message);
+      if (!reply) {
+        return Promise.resolve();
+      }
+      return event.data.ref.parent.push({
+        name: 'MadBot',
+        message: reply,
+        photoUrl: 'https://botlist.co/system/BotList/Bot/logos/000/001/890/medium/madbot.png',
+        id: -1,
+        date: new Date().valueOf()
+      });
+    } else {
+      return Promise.resolve();
+    }
+  });
+
+function getBotMessage(message) {
+  let reply = '';
+  message = message.toLowerCase();
+  if(message.includes('jo bolay')){
+    reply = '100 nihaal';
+  }
+
+  return reply;
+}
